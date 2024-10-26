b7dd38a75ba19ca6da48c75cc161ac5a686f2771
Oleg <olegpl1993@gmail.com>
2024-10-26 22:17:45 +0300

    init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,32 @@
-import http from 'http';
+import cors from 'cors';
+import express from 'express';
+import { Console } from './modules/console.module';
 
-const PORT = 4000;
-const server = http.createServer();
+const PORT = 5555;
+const app = express();
+app.use(cors());
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/', async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Hello from Backend</title>
+        </head>
+        <body>
+          <h1>Hello from Backend</h1>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    res.status(500);
+    res.end(`Error: ${error}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server start on port: ${PORT}`);
+  Console();
 });
