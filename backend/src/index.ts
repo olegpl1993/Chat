3f5ad90e35ce7bb1d52c3e05b4ca97b45d87fe74
Oleg <olegpl1993@gmail.com>
2024-10-27 22:57:40 +0200

    fix: imports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,8 @@ import cors from "cors";
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-import chatRoute from "./router/chatRoute";
-import mainRoute from "./router/mainRoute";
+import mainRoute from "./router/mainRoute.js";
+import chatRoute from "./router/chatRoute.js";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
